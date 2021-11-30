483ca561ada06e8d6f0cc2e16b9a19b5a711b5ce
farbod2u <saeedsaf@gmail.com>
2021-11-30 08:22:57 +0330

    add DeveloperController to vcs

---
+++ src/main/java/com/pinguinag/issuetracker/controller/DeveloperController.java
@@ -0,0 +1,38 @@
+package com.pinguinag.issuetracker.controller;
+
+import com.pinguinag.issuetracker.entity.Developer;
+import com.pinguinag.issuetracker.service.DeveloperService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/developers")
+public class DeveloperController {
+
+    private final DeveloperService developerService;
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developerService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Developer get(@PathVariable Integer id) {
+        return developerService.get(id);
+    }
+
+    @PostMapping
+    public Developer save(@RequestBody Developer entity) {
+        return developerService.save(entity);
+    }
+
+    @PutMapping
+    public Developer update(@RequestBody Developer entity) {
+        return developerService.update(entity);
+    }
+}
